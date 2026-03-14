f162a9c56c13c91612997b639563bf6e4061bae0
manojsharma248 <35368092+manojsharma248@users.noreply.github.com>
2026-03-14 16:40:29 +0530

    Refactor tests to implement missing cases and improve rendering logic

--- src/examples/counter/counter.test.tsx
+++ src/examples/counter/counter.test.tsx
@@ -1,10 +1,19 @@
-import { screen, render } from '@testing-library/react';
+// @vitest-environment happy-dom
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Counter from '.';
+import { render } from './test/utilities';
 
-test.todo('it should render the component', () => {});
+test('it should render the component', () => {
+  render(<Counter />);
+});
 
-test.todo(
-  'it should increment when the "Increment" button is pressed',
-  async () => {},
-);
+test('it should increment when the "Increment" button is pressed', async () => {
+  const { user } = render(<Counter />);
+  const currentCount = screen.getByTestId('current-count');
+  expect(currentCount).toHaveTextContent('0');
+  const incrementButton = screen.getByRole('button', { name: 'Increment' });
+  await user.click(incrementButton);
+  expect(currentCount).toHaveTextContent('1');
+  screen.debug();
+});
--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -10,19 +10,36 @@ it('has the correct title', async () => {
   screen.getByText('Packing List');
 });
 
-it.todo('has an input field for a new item', () => {});
+it('has an input field for a new item', () => {
+  render(<PackingList />);
+  screen.getByLabelText('New Item Name');
+});
 
-it.todo(
-  'has a "Add New Item" button that is disabled when the input is empty',
-  () => {},
-);
+it('has a "Add New Item" button that is disabled when the input is empty', () => {
+  render(<PackingList />);
+  const addNewItemButton = screen.getByRole('button', {
+    name: 'Add New Item',
+  });
+  expect(addNewItemButton).toBeDisabled();
+});
 
-it.todo(
-  'enables the "Add New Item" button when there is text in the input field',
-  async () => {},
-);
+it('enables the "Add New Item" button when there is text in the input field', async () => {
+  const { user } = render(<PackingList />);
+  const newItemInput = screen.getByPlaceholderText('New Item');
+  await user.type(newItemInput, 'Toothbrush');
+  const addNewItemButton = screen.getByRole('button', {
+    name: 'Add New Item',
+  });
+  expect(addNewItemButton).toBeEnabled();
+});
 
-it.todo(
-  'adds a new item to the unpacked item list when the clicking "Add New Item"',
-  async () => {},
-);
+it('adds a new item to the unpacked item list when the clicking "Add New Item"', async () => {
+  const { user } = render(<PackingList />);
+  const newItemInput = screen.getByPlaceholderText('New Item');
+  await user.type(newItemInput, 'MacBook Pro');
+  const addNewItemButton = screen.getByRole('button', {
+    name: 'Add New Item',
+  });
+  await user.click(addNewItemButton);
+  expect(screen.getByLabelText('MacBook Pro')).not.toBeChecked();
+});
