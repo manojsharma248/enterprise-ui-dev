7ec85c49ef6f34cd60f1ad7f1cf3ae12451df405
manojsharma248 <35368092+manojsharma248@users.noreply.github.com>
2026-03-14 18:06:18 +0530

    Refactor PackingList component and update tests to use Redux store

--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -1,6 +1,18 @@
-import { render, screen } from 'test/utilities';
-import PackingList from '.';
+import { render as baseRender, screen } from 'test/utilities';
+import { PackingList } from '.';
+import { Provider } from 'react-redux';
+import { createStore } from './store';
+import { PropsWithChildren } from 'react';
 
+const render: typeof baseRender = (Component, options) => {
+  const store = createStore();
+
+  const Wrapper = ({ children }: PropsWithChildren) => {
+    return <Provider store={store}>{children}</Provider>;
+  };
+
+  return baseRender(Component, { ...options, wrapper: Wrapper });
+};
 it('renders the Packing List application', () => {
   render(<PackingList />);
 });
@@ -42,4 +54,36 @@ it('adds a new item to the unpacked item list when the clicking "Add New Item"',
   });
   await user.click(addNewItemButton);
   expect(screen.getByLabelText('MacBook Pro')).not.toBeChecked();
+});
+it('adds a new item to the unpacked item list when the clicking "Add New Item"', async () => {
+  const { user } = render(<PackingList />);
+  const newItemInput = screen.getByPlaceholderText('New Item');
+  await user.type(newItemInput, 'MacBook Pro');
+  const addNewItemButton = screen.getByRole('button', {
+    name: 'Add New Item',
+  });
+  await user.click(addNewItemButton);
+  expect(screen.getByLabelText('MacBook Pro')).not.toBeChecked();
+});
+
+// This test is sublty flawed.
+it('removes an item when the remove button is clicked', async () => {
+  const { user } = render(<PackingList />);
+
+  const newItemInput = screen.getByLabelText<HTMLInputElement>('New Item Name');
+  const addNewItemButton = screen.getByRole<HTMLButtonElement>('button', {
+    name: 'Add New Item',
+  });
+
+  await user.type(newItemInput, 'iPad Pro');
+  await user.click(addNewItemButton);
+
+  const item = screen.getByLabelText('iPad Pro');
+  const removeButton = screen.getByRole('button', {
+    name: 'Remove iPad Pro',
+  });
+
+  await user.click(removeButton);
+
+  expect(item).not.toBeInTheDocument();
 });
